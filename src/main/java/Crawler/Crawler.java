6beba041194bb8de93c650a8e16da66b9acf6b9c
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-14 11:31:40 +0200

    Refactor and add robots w/o wildcards

--- src/main/java/Crawler/Crawler.java
+++ src/main/java/Crawler/Crawler.java
@@ -1,24 +1,20 @@
 package Crawler;
 
-import Crawler.CrawlerUtils.*;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashSet;
-import java.util.Objects;
+
+import static Constants.Constants.*;
 
 public class Crawler {
 
-    public static String userAgent = "Crawler3505";
-    public static Integer MAX_LINKS_PER_DOC = 3, MAX_DOCS = 10;
 
     public static void crawl() throws IOException {
         HashSet<String> visited = new HashSet<>();
-        HashSet<String> toVisit = CrawlerUtils.ReadInitialSeed("src/main/resources/InitialSeed.txt");
+        HashSet<String> toVisit = CrawlerUtils.ReadInitialSeed(SEED_PATH);
 
         // while there are still links to visit
         // and the visited links haven't exceeded the limit
@@ -26,6 +22,8 @@ public class Crawler {
 
             String url = toVisit.iterator().next();
             toVisit.remove(url);
+
+            url = CrawlerUtils.NormalizeUrl(url);
             if (! visited.contains(url)) {
                 crawlURL(url, visited, toVisit);
                 visited.add(url);
@@ -40,31 +38,12 @@ public class Crawler {
     }
     public static void crawlURL(String url, HashSet<String> visited, HashSet<String> toVisit) {
         // reading HTML document from given URL
-        Document document;
-        try {
-            Connection connection = Jsoup.connect(url).timeout(10000).maxBodySize(0).userAgent(userAgent);
-
-            // get domain name of website
-
-            Connection.Response response = connection.execute();
-
-//            System.out.println(response.contentType());
-            if (response.contentType() != null && !Objects.requireNonNull(response.contentType()).contains("text/html")) {
-                return;
-            }
-
-            document = connection.get();
-        } catch (IOException e) {
-            System.out.println("Error: " );
-            e.printStackTrace();
-            return;
-        }
 
         // Check for Robots.txt
 
         // getting all links from document
-        Elements links = document.getElementsByTag("a");
-
+        Elements links = CrawlerUtils.GetAllLinks(url);
+        if (links == null) return;
 //        System.out.println("Links: " + links.size());
 
         HashSet<String> newLinks = new HashSet<>();
@@ -75,7 +54,7 @@ public class Crawler {
             if (relative_link.startsWith("#") || relative_link.isEmpty())
                 continue;
 
-            String abs_link = link.attr("abs:href").toLowerCase();
+            String abs_link = CrawlerUtils.NormalizeUrl(link.attr("abs:href").toLowerCase());
             if (!abs_link.startsWith("http")) continue;
 
             // check if link already visited or forbidden by robots.txt
@@ -92,6 +71,11 @@ public class Crawler {
     }
 
     public static void main(String[] args) throws IOException {
-        crawl();
+//        crawl();
+        for (int i = 0; i < 10; i++) {
+            URL x = new URL("https://www.google.com");
+            x.openConnection();
+            System.out.println(x.getHost());
+        }
     }
 }
--- src/main/java/Crawler/RobotsChecker.java
+++ src/main/java/Crawler/RobotsChecker.java
@@ -5,25 +5,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static Constants.Constants.*;
+
 
 public class RobotsChecker {
 
-    public static String userAgent = "Crawler3505";
-
-    public boolean check(String url) {
-        return true;
+    enum RobotPermissionTypes {
+        WILDCARDS,
+        STRAIGHT_MATCHES
     }
 
-    public void getRobotsTxtContent(String incomingUrl) throws IOException {
+    public HashMap<String, HashMap<RobotPermissionTypes, HashSet<String>>> disallowed = new HashMap<>();
+    public HashMap<String, HashMap<RobotPermissionTypes, HashSet<String>>> allowed = new HashMap<>();
 
-        // get robots.txt url
+    public boolean check(String url) {
+        URL urlObject;
+
+        try {
+            urlObject = new URL(url);
+        } catch (MalformedURLException e) {
+            return false;
+        }
+
+        try {
+            // if site not visited before, check the robots.txt
+            if (!allowed.containsKey(urlObject.getHost())) getRobotsTxtContent(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        HashMap<RobotPermissionTypes, HashSet<String>> allowMap = allowed.get(urlObject.getHost());
+        HashMap<RobotPermissionTypes, HashSet<String>> disallowMap = disallowed.get(urlObject.getHost());
+
+        // Allowed : Straight matches
+        HashSet<String> allowStraightMatches = allowMap.get(RobotPermissionTypes.STRAIGHT_MATCHES);
+        for (String allowStraightMatch : allowStraightMatches) {
+            if (url.contains(allowStraightMatch)) return true;
+        }
+
+        // Allowed : Wildcards
+        HashSet<String> allowWildcards = allowMap.get(RobotPermissionTypes.WILDCARDS);
+        // don't know for now
+        for (String allowWildcard : allowWildcards) {
+            continue;
+        }
+
+
+        // Disallowed : Straight matches
+        HashSet<String> disallowStraightMatches = disallowMap.get(RobotPermissionTypes.STRAIGHT_MATCHES);
+        for (String disallowStraightMatch : disallowStraightMatches) {
+            if (url.contains(disallowStraightMatch)) return false;
+        }
+
+        // Disallowed : Wildcards
+        HashSet<String> disallowWildcards = disallowMap.get(RobotPermissionTypes.WILDCARDS);
+        // don't know for now
+        for (String disallowWildcard : disallowWildcards) {
+            continue;
+        }
+
+        return true;
+
+    }
+
+    public BufferedReader getRobotsFile(String incomingUrl) {
         URL url;
 
         try {
             url = new URL(incomingUrl);
         } catch (MalformedURLException e) {
             e.printStackTrace();
-            return;
+            return null;
         }
 
         String robotsTxtUrl = url.getProtocol() + "://" + url.getHost() + "/robots.txt";
@@ -41,11 +96,41 @@ public class RobotsChecker {
 
         } catch (IOException e) {
             e.printStackTrace();
-            return;
+            return null;
         }
 
-        String line;
+        return in;
+    }
 
+    public void initializeMap(HashMap<String, HashMap<RobotPermissionTypes, HashSet<String>>> map, String host) {
+        map.put(host, new HashMap<>());
+        map.get(host).put(RobotPermissionTypes.WILDCARDS, new HashSet<>());
+        map.get(host).put(RobotPermissionTypes.STRAIGHT_MATCHES, new HashSet<>());
+    }
+
+    public void getRobotsTxtContent(String incomingUrl) throws IOException {
+
+        // if already checked, return
+        URL incomingUrlObject = new URL(incomingUrl);
+        String host = incomingUrlObject.getHost();
+
+        if (allowed.containsKey(host) || disallowed.containsKey(host)) return;
+        // create empty hash map in case of no robots.txt file
+        // so that we don't visit it again
+
+        initializeMap(disallowed, host);
+        initializeMap(allowed, host);
+
+        HashMap<RobotPermissionTypes, HashSet<String>> hostDisallowedMap = disallowed.get(host);
+        HashMap<RobotPermissionTypes, HashSet<String>> hostAllowedMap = allowed.get(host);
+
+        // get robots.txt url
+        // BufferedReader outputs the content of the robots.txt file
+        // line by line
+        BufferedReader in = getRobotsFile(incomingUrl);
+        if (in == null) return;
+
+        String line;
         while ((line = in.readLine()) != null) {
 
             if (line.startsWith("User")) {
@@ -57,11 +142,30 @@ public class RobotsChecker {
 
                     while ((line = in.readLine()) != null) {
                         if (line.startsWith("Disallow:")) {
-                            String disallow = line.split(":")[1].trim();
-                            System.out.println("Disallow : " + disallow);
+                            String disallowString = line.split(":")[1].trim();
+                            String disallowedUrl = incomingUrlObject.getProtocol() + "://" + incomingUrlObject.getHost() + disallowString;
+                            System.out.println("Disallow : " + disallowString);
+
+                            // if it is a straight match, we add it to the straight hash set
+                            if (true) {
+
+                                hostDisallowedMap.get(RobotPermissionTypes.STRAIGHT_MATCHES).add(disallowedUrl);
+                            } else {
+                                hostDisallowedMap.get(RobotPermissionTypes.WILDCARDS).add(disallowedUrl);
+                            }
                         } else if (line.startsWith("Allow:")) {
-                            String allow = line.split(":")[1].trim();
-                            System.out.println("Allow : " + allow);
+                            String allowString = line.split(":")[1].trim();
+                            System.out.println("Allow : " + allowString);
+                            String allowedUrl = incomingUrlObject.getProtocol() + "://" + incomingUrlObject.getHost() + allowString;
+                            System.out.println("Disallow : " + allowString);
+
+                            // if it is a straight match, we add it to the straight hash set
+                            if (true) {
+
+                                hostAllowedMap.get(RobotPermissionTypes.STRAIGHT_MATCHES).add(allowedUrl);
+                            } else {
+                                hostAllowedMap.get(RobotPermissionTypes.WILDCARDS).add(allowedUrl);
+                            }
                         } else if (line.startsWith("User")) {
                             // if there are robots for other agents, we ignore them
                             agent = line.split(":")[1].trim();
