0897eda095b9ef7aa6b1a888fe8881406b74acdb
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-14 11:30:37 +0200

    Add constant file

---
+++ src/main/java/Constants/Constants.java
@@ -0,0 +1,12 @@
+package Constants;
+
+public final class Constants {
+    private Constants() {
+    }
+
+
+    public static final String userAgent = "Crawler3505";
+    public static final String SEED_PATH = "src/main/resources/InitialSeed.txt";
+
+    public static Integer MAX_LINKS_PER_DOC = 3, MAX_DOCS = 10;
+}
