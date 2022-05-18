5158d4967793d92eb02891ecf067a8fa3d22a18c
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-05-18 18:41:53 +0200

    response object structure on fe

--- Frontend/src/app/ResponesObjects.ts
+++ Frontend/src/app/ResponesObjects.ts
@@ -1,5 +1,10 @@
+
 export class ResponseObject{
     formattedUrl: string = "www.google.com";
     title: string = "Ahmed Ehab ";
     snippet: string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever."
+}
+
+export class ResponseObjects{
+    list: ResponseObject[] = [];
 }
