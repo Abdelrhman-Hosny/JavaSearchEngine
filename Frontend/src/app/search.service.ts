f89ff356dbf0f2105cb01bcfe8e0ff5be517c968
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-05-18 17:30:12 +0200

    removed google response

--- Frontend/src/app/search.service.ts
+++ Frontend/src/app/search.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../environments/environment";
-import {GoogleResponse} from "./GoogleResponse.model";
 import { Observable} from "rxjs";
 import{map} from 'rxjs/operators'
 import { AutoCompleteObject } from './AutoCompleteObject';
