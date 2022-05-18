5d1225def85fd64f569cc99758b41d8fb62e2686
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-18 07:10:33 +0200

    add pageDegree constant

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     public static final String userAgent = "Crawler3505";
     public static final String SEED_PATH = "src/main/resources/InitialSeed.txt";
 
-    public static final Integer MAX_LINKS_PER_DOC = 200, MAX_DOCS = 20;
+    public static final Integer MAX_LINKS_PER_DOC = 200, MAX_DOCS = 30;
 
     public static final String DOWNLOAD_PATH = "src/main/resources/CrawlerHTML/";
 
@@ -17,4 +17,5 @@ public final class Constants {
 
     public static final String VISITED_SAVE_FILE = "visitedURLs.txt";
     public static final String TO_VISIT_SAVE_FILE = "toVisitURLs.txt";
+    public static final String PAGE_DEGREE_SAVE_FILE = "pageDegree.txt";
 }
