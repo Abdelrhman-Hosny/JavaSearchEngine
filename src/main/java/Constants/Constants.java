941150accecabd590919350e42a9ea1523dc7c2f
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-18 05:56:31 +0200

    make the default to crawl 20 documents

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     public static final String userAgent = "Crawler3505";
     public static final String SEED_PATH = "src/main/resources/InitialSeed.txt";
 
-    public static final Integer MAX_LINKS_PER_DOC = 200, MAX_DOCS = 500;
+    public static final Integer MAX_LINKS_PER_DOC = 200, MAX_DOCS = 20;
 
     public static final String DOWNLOAD_PATH = "src/main/resources/CrawlerHTML/";
 
