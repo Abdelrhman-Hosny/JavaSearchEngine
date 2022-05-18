a369a9517da5c0c509141254a9ac5d7a5c363800
yousefQadry <58371826+yousefQadry@users.noreply.github.com>
2022-05-19 01:13:36 +0200

    modified getPhraseLevel function

--- src/main/java/PhraseLevel/PhraseLevel.java
+++ src/main/java/PhraseLevel/PhraseLevel.java
@@ -8,11 +8,18 @@ import java.util.HashMap;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import Ranker.Ranker.Entry;
 import Utils.Utils;
 
 public class PhraseLevel {
 
-    public ArrayList<Integer> getPhraseLevel(ArrayList<String> urlArray, String sentence) throws IOException {
+    public ArrayList<Integer> getPhraseLevel(ArrayList<Entry> urlArrayEntry, String sentence) throws IOException {
+        
+        ArrayList<String> urlArray = new ArrayList<>();
+        for(int i=0;i<urlArrayEntry.size();i++)
+        {
+            urlArray.add(urlArrayEntry.get(i).getKey());
+        }
 
         Utils utils = new Utils();
         HashMap<String, String> urlPathMap = utils.getUrlPathMap();
@@ -38,7 +45,7 @@ public class PhraseLevel {
 
         ArrayList<String> documents = new ArrayList<>();
         String sentence = "";
-
+        
         // run get phrase level
         ArrayList<Integer> takenIndices = getPhraseLevel(documents, sentence);
 
