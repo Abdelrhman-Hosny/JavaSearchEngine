538725bf03e16714623f2cab8f949441f077f444
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-05-18 20:30:46 +0200

    made rankerdao

--- src/main/java/Database/RankerDAO.java
+++ src/main/java/Database/RankerDAO.java
@@ -47,4 +47,67 @@ public class RankerDAO extends BaseDAO {
         return null;
     }
 
+    public Integer getTotalNumberOfDocument() {
+        CallableStatement cstmt;
+        try {
+
+
+            cstmt = connection.prepareCall("{call totalNumberOfDocuments}");
+            ResultSet rs =  cstmt.executeQuery(); // return resultSet
+            int result = 0;
+            while(rs.next()){
+                result = rs.getInt("numberOfDocuments");
+            }
+            rs.close();
+            return result;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ResultSet GetDocumentsWhereAllWordsAppears(String finalWords){
+        String queryGetDocumentsWhereAllWordsAppears = "SELECT  document_name" +
+        " FROM IndexTable" + " where word in " + finalWords ;
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            return stmt.executeQuery(queryGetDocumentsWhereAllWordsAppears);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return (ResultSet) e;
+        }
+    }
+    public void addPageRank(HashMap<String,Double> pageRank){
+        try {
+            // since we can do incremental update
+            // so we need to check that we delete url before working due to some reasons:
+            // as page is updated and maybe words are outdated
+            // will have same (word,url) combination which will damage primary key constraints
+            CallableStatement cstmt;
+            cstmt = connection.prepareCall("{call addPageRank(?,?)}");
+            for (String key : pageRank.keySet()) {
+
+                cstmt.setString(1, key);
+                cstmt.setDouble(2, pageRank.get(key));
+
+                if(cstmt.execute()){
+                    cstmt.close();
+                }
+            }
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        RankerDAO rk = new RankerDAO();
+        System.out.println(rk.getTotalNumberOfDocument());
+    }
 }
