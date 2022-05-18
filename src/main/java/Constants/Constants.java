8cf1875a2de278eaff3af23523158dfe6e1e0ba8
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-05-18 05:43:59 +0200

    made numthreads as a constant

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -11,4 +11,8 @@ public final class Constants {
     public static final Integer MAX_LINKS_PER_DOC = 200, MAX_DOCS = 10;
 
     public static final String DOWNLOAD_PATH = "src/main/resources/CrawlerHTML/";
+
+    public static final double spamPercentage = 0.4; // if certain word exceed that percentage report spam
+    public static final int documentSizeThreshold = 100;
+    public static final int numThreadsIndexer = 5;
 }
