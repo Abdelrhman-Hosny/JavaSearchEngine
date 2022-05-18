1f653cc4c4a522be8611e26960578c22e9609553
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-05-18 21:41:04 +0200

    removed prints

--- src/main/java/Ranker/documentRank.java
+++ src/main/java/Ranker/documentRank.java
@@ -38,12 +38,7 @@ public class documentRank implements Runnable {
             String currentDocument = shareOfDocs.get(k);
 
             for (int i = 0; i < searchQuery.length; i++) {
-                //getting tf of word
-                // String queryTf = "SELECT title,h1,h2,h3,h4,bold,text,tf_idf as tf" +
-                //         " FROM " + Ranker.documentWordTable +
-                //         " where word = '" + searchQuery[i] + "'" +
-                //         " and document_name = '" + currentDocument + "'";
-                //System.out.println(queryTf);
+                
                 ResultSet rsWordTF = rankerDB.SearchWordIndex(searchQuery[i], currentDocument);
                 int h1 = 0;
                 int h2 = 0;
@@ -73,9 +68,6 @@ public class documentRank implements Runnable {
                     }
                 }
                 int finalWeights = titleWeight*title + h1Weight*h1 + h2Weight*h2 + h3Weight*h3 + h4Weight*h4 + boldWeight*bold + textWeight*text;
-                System.out.println("weights are " + finalWeights);
-                System.out.println("Current word tf is " + current_tf);
-                System.out.println("WordTF after weights is " + current_tf*finalWeights);
                 current_tf = current_tf*finalWeights;
                 //------------------------------------------------------------------------
                 //getting idf of word
@@ -84,11 +76,9 @@ public class documentRank implements Runnable {
                 //--------------------------------------------------------------------------
                 double idfOfWord = Math.log((double) countTotalNumberOfDocuments / (double) countWordInWeb);
                 tf_idf += (current_tf * idfOfWord);
-                System.out.println("Current TF-idf is" + tf_idf);
             }
             synchronized (rankerResult) {
                 rankerResult.add(new Ranker.Entry(currentDocument, tf_idf)); //adding to priority queue
-
             }
 
         }
